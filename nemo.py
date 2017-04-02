967547038c5c74eb005dc7a718ee009c242944bb
marcvincenti <vincentimarc@gmail.com>
2017-04-02 10:12:46 +0200

    update api

--- nemo.py
+++ nemo.py
@@ -1,28 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
 # INSTALL & START
 #
 # pip install Flask flask-cors
-# ngrok http -subdomain=nemo 8080
 # python nemo.py
 
 app = Flask("Nemo")
 CORS(app)
-val = False
+val = 0
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    global val
-    res = { 'state' : val}
-    return jsonify(res)
-
-@app.route("/hackhim")
-def hackhim():
-    global val
-    val = not val
-    res = { 'pownedTo' : val}
-    # request.args.get('para1SiDansURL') <= pour recup' un param
-    return jsonify(res)
+	global val
+	if request.method == 'POST':
+		val = request.args.get('state')
+	res = { 'state' : val }
+	return jsonify(res)
 
 app.run(debug=True, host='0.0.0.0', port=8080)
